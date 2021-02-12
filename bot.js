aac1c5e690fb4bd43aca40b0e9a7ad0d18459d68
Melchior Masset <61066105+melchiormass@users.noreply.github.com>
2021-02-12 23:04:17 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('Le bot est prêt !');
+})
+
+
+client.on('message', (message) => {
+    const channel = message.channel;
+    const members = channel.members;
+
+    if(message.content == '!muteall'){
+        members.forEach(member =>{
+            member.voice.setMute(true);
+            member.voice.setDeaf(true);
+            message.channel.send('Les joueurs ont été mutés !')
+        });
+    }else if (message.content == '!unmuteall'){
+        members.forEach(member =>{
+            member.voice.setMute(false);
+            member.voice.setDeaf(false);
+            message.channel.send('Les joueurs ont été démutés !')
+        });
+    }
+});
+
+
+client.login('ODA5ODY4MjUwMDI4MTc5NTU2.YCbWlQ.uX6JvtVMdnBjbd6na9C-qgcmBrE');
